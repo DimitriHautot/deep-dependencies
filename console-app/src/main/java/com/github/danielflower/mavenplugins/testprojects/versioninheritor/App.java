e4184214b878a2b7a6af57b040a2bdb06b11c7dd
Dimitri Hautot <3765206+DimitriHautot@users.noreply.github.com>
2021-02-12 11:53:37 +0100

    Changed arithmetic computation

--- console-app/src/main/java/com/github/danielflower/mavenplugins/testprojects/versioninheritor/App.java
+++ console-app/src/main/java/com/github/danielflower/mavenplugins/testprojects/versioninheritor/App.java
@@ -3,6 +3,6 @@ package com.github.danielflower.mavenplugins.testprojects.versioninheritor;
 public class App {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println("1 + 2 = " + calculator.add(1, 2));
+        System.out.println("0 + 42 = " + calculator.add(0, 42));
     }
 }
